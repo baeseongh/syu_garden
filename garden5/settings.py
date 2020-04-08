137564241b38345d54d8b9aa0847c6dc9a9dcc3d
seonghyeon <tjdgusdkf@gmail.com>
2020-04-09 01:11:29 +0900

    up

--- garden5/settings.py
+++ garden5/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9pvo@2w)46+)cvbdmtltg*w$eq#sgs2k@qrwd6x6kcf&y5*&hk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
